aa602d2b119157ba8a63a55cb27c6a840d05f637
Soumita Mukherjee <146662360+soumita654@users.noreply.github.com>
2025-04-23 22:50:35 +0530

    Update data_generator.py

--- data_generator.py
+++ data_generator.py
@@ -1,15 +1,13 @@
+!pip install fireducks
 import pandas as pd
+import fireducks.pandas as pd
 from sklearn.datasets import make_classification
-from src.config import NUM_SAMPLES, NUM_FEATURES, SEED, DATA_PATH
 
-def generate_and_save_data():
-    X, y = make_classification(
-        n_samples=NUM_SAMPLES,
-        n_features=NUM_FEATURES,
-        random_state=SEED
-    )
-    columns = [f"feature_{i}" for i in range(NUM_FEATURES)]
-    df = pd.DataFrame(X, columns=columns)
-    df["target"] = y
-    df.to_parquet(DATA_PATH)
-    print(f"Dataset saved to {DATA_PATH}")
+X, y = make_classification(n_samples=10_000_000, n_features=20, random_state=42)
+columns = [f'feature_{i}' for i in range(20)]
+
+df_pd = pd.DataFrame(X, columns=columns)
+df_pd['target'] = y
+
+# Save as Parquet (used for both tools)
+df_pd.to_parquet('benchmark_dataset.parquet')
